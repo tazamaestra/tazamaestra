04cec18d3f30adce0bb5efd30e8474ac2706fa9c
tazamaestra <maestrataza@gmail.com>
2025-02-05 08:08:39 -0500

    modificando index.js

--- index.js
+++ index.js
@@ -2,17 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import { assistantRoute } from './routes/assistants.js';
 import chatRouter from './routes/chat.js';
-import dotenv from 'dotenv';
 
 
-// Configuramos dotenv y guardamos el resultado
-const result = dotenv.config();
-
-// Verificamos si hubo error al cargar el .env
-if (result.error) {
-  console.log('Error al cargar .env:', result.error);
-}
-const port = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -26,6 +17,4 @@ app.use((err, req, res, next) => {
   res.status(500).send('¡Algo salió mal!');
 });
 
-app.listen(port, () => {
-  console.log(`Servidor corriendo en puerto ${port}`);
-});
+app.listen();
